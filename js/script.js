8bc24bb3d7e897a03d4298d14ff45fe3a8d965d8
Jean Claro <jeanclarodev@gmail.com>
2024-03-24 22:01:17 -0300

    Finish

--- js/script.js
+++
@@ -1,9 +0,0 @@
-document.getElementById('comoFuncionaBtn').addEventListener('click', scrollToSection);
-
-function scrollToSection() {
-    window.scrollTo({
-        top: document.getElementById('Function').offsetTop,
-        behavior: 'smooth'
-    });
-}
-
